44d25b92cf9b442c1ddf0fcedd91ae999dd7ea5c
GuiGWR <60243589+GuiGWR@users.noreply.github.com>
2021-05-02 16:11:44 -0400

    Restaurant Details fixed + CommentairesActivity.kt

---
+++ Final/app/src/main/java/ca/ulaval/ima/mp/ui/commentaires/CommentairesActivity.kt
@@ -0,0 +1,12 @@
+package ca.ulaval.ima.mp.ui.commentaires
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ca.ulaval.ima.mp.R
+
+class CommentairesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_commentaires)
+    }
+}
